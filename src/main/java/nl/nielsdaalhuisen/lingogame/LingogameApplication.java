a85b12f68b388f93d16a72e5c6d5047a1730452c
Niels Daalhuisen <niels.daalhuisen@kubion.nl>
2020-08-16 15:24:27 +0200

    spring project

---
+++ src/main/java/nl/nielsdaalhuisen/lingogame/LingogameApplication.java
@@ -0,0 +1,13 @@
+package nl.nielsdaalhuisen.lingogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingogameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingogameApplication.class, args);
+    }
+
+}
